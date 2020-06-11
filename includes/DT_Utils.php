f840448acf415ad711d12de29ae7ccd93a781375
Yaron Koren <yaron57@gmail.com>
2020-06-11 15:56:25 +0000

    Modify HTML creation in DT_Utils.php
    Change-Id: Ia313478f803dd66ee7639329ff1f024deeaf5cf6

--- includes/DT_Utils.php
+++ includes/DT_Utils.php
@@ -8,11 +8,11 @@
 class DTUtils  {
 
 	static function printImportingMessage() {
-		return "\t" . Xml::element( 'p', null, wfMessage( 'dt_import_importing' )->text() ) . "\n";
+		return "\t" . Html::element( 'p', null, wfMessage( 'dt_import_importing' )->text() ) . "\n";
 	}
 
 	static function printFileSelector( $fileType ) {
-		$text = "\n\t" . Xml::element( 'p', null, wfMessage( 'dt_import_selectfile', $fileType )->text() ) . "\n";
+		$text = "\n\t" . Html::element( 'p', null, wfMessage( 'dt_import_selectfile', $fileType )->text() ) . "\n";
 		$text .= <<<END
 	<p><input type="file" name="file_name" size="25" /></p>
 
@@ -21,66 +21,50 @@ END;
 		return $text;
 	}
 
+	static function printImportRadioButton( $option, $optionMsg, $isChecked = false) {
+		$radioButtonAttrs = [];
+		if ( $isChecked ) {
+			$radioButtonAttrs['checked'] = true;
+		}
+		$text = "\t" . Html::input( 'pagesThatExist', $option, 'radio', $radioButtonAttrs ) .
+			"\t" . wfMessage( $optionMsg )->text() . "<br />" . "\n";
+		$text = Html::rawElement( 'label', null, $text ) . "\n";
+		return $text;
+	}
+
 	static function printExistingPagesHandling() {
-		$text = "\t" . Xml::element( 'p', null, wfMessage( 'dt_import_forexisting' )->text() ) . "\n";
-		$existingPagesText = "\n\t" .
-			Xml::element( 'input',
-				[
-					'type' => 'radio',
-					'name' => 'pagesThatExist',
-					'value' => 'overwrite',
-					'checked' => 'checked'
-				] ) . "\n" .
-			"\t" . wfMessage( 'dt_import_overwriteexisting' )->text() . "<br />" . "\n" .
-			"\t" . Xml::element( 'input',
-				[
-					'type' => 'radio',
-					'name' => 'pagesThatExist',
-					'value' => 'merge',
-				] ) . "\n" .
-			"\t" . wfMessage( 'dt_import_mergeintoexisting' )->text() . "<br />" . "\n\t" .
-			"\t" . Xml::element( 'input',
-				[
-					'type' => 'radio',
-					'name' => 'pagesThatExist',
-					'value' => 'skip',
-				] ) . "\n" .
-			"\t" . wfMessage( 'dt_import_skipexisting' )->text() . "<br />" . "\n" .
-			"\t" . Xml::element( 'input',
-				[
-					'type' => 'radio',
-					'name' => 'pagesThatExist',
-					'value' => 'append',
-				] ) . "\n" .
-			"\t" . wfMessage( 'dt_import_appendtoexisting' )->text() . "<br />" . "\n\t";
-		$text .= "\t" . Xml::tags( 'p', null, $existingPagesText ) . "\n";
+		$text = "\t" . Html::element( 'p', null, wfMessage( 'dt_import_forexisting' )->text() ) . "\n";
+		$existingPagesText =
+			self::printImportRadioButton( 'overwrite', 'dt_import_overwriteexisting', true ) .
+			self::printImportRadioButton( 'merge', 'dt_import_mergeintoexisting' ) .
+			self::printImportRadioButton( 'skip', 'dt_import_skipexisting' ) .
+			self::printImportRadioButton( 'append', 'dt_import_appendtoexisting' );
+		$text .= "\t" . Html::rawElement( 'p', null, $existingPagesText ) . "\n";
 		$text .= "\t" .  '<hr style="margin: 10px 0 10px 0" />' . "\n";
 		return $text;
 	}
 
 	static function printImportSummaryInput( $fileType ) {
-		$importSummaryText = "\t" . Xml::element( 'input',
+		$importSummaryText = "\t" . Html::input(
+			'import_summary',
+			wfMessage( 'dt_import_editsummary', $fileType )->inContentLanguage()->text(),
+			'text',
 			[
-				'type' => 'text',
 				'id' => 'wpSummary', // ID is necessary for CSS formatting
 				'class' => 'mw-summary',
-				'name' => 'import_summary',
-				'value' => wfMessage( 'dt_import_editsummary', $fileType )->inContentLanguage()->text()
 			]
 		) . "\n";
-		return "\t" . Xml::tags( 'p', null,
+		return "\t" . Html::rawElement( 'p', null,
 			wfMessage( 'dt_import_summarydesc' )->text() . "\n" .
 			$importSummaryText ) . "\n";
 	}
 
 	static function printSubmitButton() {
-		$formSubmitText = Xml::element( 'input',
-			[
-				'type' => 'submit',
-				'name' => 'import_file',
-				'value' => wfMessage( 'import-interwiki-submit' )->text()
-			]
+		$formSubmitText = Html::input(
+			'import_file',
+			wfMessage( 'import-interwiki-submit' )->text(),
+			'submit'
 		);
-		return "\t" . Xml::tags( 'p', null, $formSubmitText ) . "\n";
+		return "\t" . Html::rawElement( 'p', null, $formSubmitText ) . "\n";
 	}
 }
